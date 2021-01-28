ec2668b06c36c423fb65dd9fc2ea8e9a9a142673
ymkashif <74010209+ymkashif@users.noreply.github.com>
2021-01-28 23:02:31 +0530

    Add files via upload

---
+++ particle.js
@@ -0,0 +1,34 @@
+function Particle(x, y, r) {
+    this.hue = random(360);
+    var options = {
+      restitution: 0.5,
+      friction: 0,
+      density: 1,
+    }
+    x += random(-1, 1);
+    this.body = Bodies.circle(x, y,this.r, options);
+    this.color=color(random(0,255),random(0,255),random(0,255));
+    World.add(world, this.body);
+  }
+  
+  Particle.prototype.isOffScreen = function() {
+    var x = this.body.position.x;
+    var y = this.body.position.y;
+    return (x < -50 || x > width + 50 || y > height);
+  }
+  
+  Particle.prototype.show = function() {
+    fill(this.hue, 255, 255);
+    noStroke();
+    var pos = this.body.position;
+    push();
+    translate(pos.x, pos.y);
+    ellipse(0, 0, this.r * 2);
+    pop();
+  }
+
+ 
+  
+  
+  
+  
